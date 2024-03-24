73a16d5e1f91df4d04c7c6817a17f3a0415634dc
Samuel-Brito19 <sambriton7@gmail.com>
2024-03-24 19:27:50 -0300

    Creating our routes

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const http = require('http')
 const PORT = 3000
+const DEFAULT_HEADER = {'Content-Type': 'aplication'}
 const handler = (request, response) => {
+    const {url, method} = request
+    const [first, route, id] = url.split('/')
+    request.queryString = {id: isNaN(id) ? id : Number(id)} 
+   
+    const key = `${route}: ${method.toLowerCase()}`
 
+    response.writeHead(200,DEFAULT_HEADER)
+    response.end('opa')
 }
 
 http.createServer(handler).listen(PORT, () => console.log('server running at ',PORT))
